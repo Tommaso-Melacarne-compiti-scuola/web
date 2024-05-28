02cfd25c2643fd1814deb5789d721dafd9ac1b19
PolpOnline <git@t0mmy.anonaddy.com>
2024-05-29 01:03:00 +0200

    Console.log too

--- src/bigliettiJS/js/script.js
+++ src/bigliettiJS/js/script.js
@@ -3,19 +3,23 @@ let button = document.getElementById('buy');
 button.addEventListener('click', buttonFn);
 
 function buttonFn() {
-    let age = prompt('Quanti anni hai?');
+    const age = prompt('Quanti anni hai?');
 
     if (!age || age < 0 || age > 120) {
         return buttonFn();
     }
-    
-    if (age < 5) { 
-        alert("Il biglietto è gratis");
+
+    let msg;
+    if (age < 5) {
+        msg = "Il biglietto è gratis";
     } else if (age > 18 && age < 25) {
-        alert("Il costo del biglietto è di 3.50€");
+        msg = "Il costo del biglietto è di 3.50€";
     } else if (age > 50 && age < 55) {
-        alert("Il costo del biglietto è di 5€");
+        msg = "Il costo del biglietto è di 5€";
     } else {
-        alert("Il costo del biglietto è di 8€");
+        msg = "Il costo del biglietto è di 8€";
     }
+
+    console.log(msg);
+    alert(msg);
 }
