9e81849ebc6c0d3c40a6a8c7cb6812db8d10777f
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-13 13:54:50 +0100

    Fmt chartJSAdd

--- src/chartJSAdd/js/addForm.js
+++ src/chartJSAdd/js/addForm.js
@@ -1,25 +1,26 @@
-import {updateChart} from "./chart.js";
-import {teams, updateVotesOrCreate} from "./index.js";
+import { updateChart } from "./chart.js";
+import { teams, updateVotesOrCreate } from "./index.js";
 
 document.getElementById("addTeamShowForm").addEventListener("click", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    document.getElementById("addTeamContainer").classList.remove("d-none");
-    document.getElementById("addTeamShowFormParagraph").classList.add("d-none");
+  document.getElementById("addTeamContainer").classList.remove("d-none");
+  document.getElementById("addTeamShowFormParagraph").classList.add("d-none");
 });
 
 document.getElementById("addTeamForm").addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(e.target);
+  const formData = new FormData(e.target);
 
-    const teamName = formData.get("teamName");
-    const teamVotes = formData.get("teamVotes");
-    const teamColor = formData.get("teamColor");
+  const teamName = formData.get("teamName");
+  const teamVotes = formData.get("teamVotes");
+  const teamColor = formData.get("teamColor");
 
-    const teamNameFormatted = teamName.charAt(0).toUpperCase() + teamName.slice(1).toLowerCase();
+  const teamNameFormatted =
+    teamName.charAt(0).toUpperCase() + teamName.slice(1).toLowerCase();
 
-    updateVotesOrCreate(teamNameFormatted, parseInt(teamVotes), teamColor);
+  updateVotesOrCreate(teamNameFormatted, parseInt(teamVotes), teamColor);
 
-    updateChart(teams);
+  updateChart(teams);
 });
--- src/chartJSAdd/js/index.js
+++ src/chartJSAdd/js/index.js
@@ -8,55 +8,51 @@
  * @property {number} votes - The number of votes for the team
  * @property {string} color - The color of the team
  */
-import {updateChart} from "./chart.js";
+import { updateChart } from "./chart.js";
 
 /**
  * @type {Team[]}
  */
 export let teams = [
-    {
-        name: "Inter",
-        votes: 0,
-        color: "#001E9D",
-    },
-    {
-        name: "Viola",
-        votes: 0,
-        color: "#61328C",
-    },
-    {
-        name: "Juve",
-        votes: 0,
-        color: "#FFFFFF",
-    },
-    {
-        name: "Milan",
-        votes: 0,
-        color: "#E50027",
-    },
+  {
+    name: "Inter",
+    votes: 0,
+    color: "#001E9D",
+  },
+  {
+    name: "Viola",
+    votes: 0,
+    color: "#61328C",
+  },
+  {
+    name: "Juve",
+    votes: 0,
+    color: "#FFFFFF",
+  },
+  {
+    name: "Milan",
+    votes: 0,
+    color: "#E50027",
+  },
 ];
 
 document.getElementById("votesForm").addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(e.target);
+  const formData = new FormData(e.target);
 
-    const votesUnparsed = {
-        inter: formData.get("inter"),
-        viola: formData.get("viola"),
-        juve: formData.get("juve"),
-        milan: formData.get("milan"),
-    };
+  const votesUnparsed = {
+    inter: formData.get("inter"),
+    viola: formData.get("viola"),
+    juve: formData.get("juve"),
+    milan: formData.get("milan"),
+  };
 
-    for (const [name, votes] of Object.entries(votesUnparsed)) {
-        updateVotesOrCreate(
-            name,
-            parseInt(votes) || 0,
-            undefined,
-        )
-    }
+  for (const [name, votes] of Object.entries(votesUnparsed)) {
+    updateVotesOrCreate(name, parseInt(votes) || 0, undefined);
+  }
 
-    updateChart(teams);
+  updateChart(teams);
 });
 
 /**
@@ -67,19 +63,21 @@ document.getElementById("votesForm").addEventListener("submit", (e) => {
  * @param color {string | undefined} The color of the team
  */
 export function updateVotesOrCreate(name, votes, color) {
-    const existingTeamIndex = teams.findIndex((team => team.name.toLowerCase() === name.toLowerCase()));
+  const existingTeamIndex = teams.findIndex(
+    (team) => team.name.toLowerCase() === name.toLowerCase(),
+  );
 
-    if (existingTeamIndex !== -1) {
-        // Update the existing team's votes
-        teams[existingTeamIndex].votes = votes;
-    } else {
-        // Add the new team to the array
-        teams.push({
-            name,
-            votes,
-            color: color ? color : getColorByTeamName(name),
-        });
-    }
+  if (existingTeamIndex !== -1) {
+    // Update the existing team's votes
+    teams[existingTeamIndex].votes = votes;
+  } else {
+    // Add the new team to the array
+    teams.push({
+      name,
+      votes,
+      color: color ? color : getColorByTeamName(name),
+    });
+  }
 }
 
 /**
@@ -89,6 +87,6 @@ export function updateVotesOrCreate(name, votes, color) {
  * @returns {string} The color of the team
  */
 function getColorByTeamName(name) {
-    const team = teams.find((t) => t.name.toLowerCase() === name.toLowerCase());
-    return team ? team.color : "#000000";
+  const team = teams.find((t) => t.name.toLowerCase() === name.toLowerCase());
+  return team ? team.color : "#000000";
 }
